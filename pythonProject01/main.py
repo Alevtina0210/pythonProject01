74d13330c291f08c7c630cd04d0f0e60826cfdb5
Alevtina0210 <alevtina.dolbilova@mail.ru>
2024-06-06 20:03:16 +0400

    Add files via upload

---
+++ pythonProject01/main.py
@@ -0,0 +1,21 @@
+a = 'Good day!'
+print(len(a))
+
+first = 8
+second = 5
+summa = first + second
+diff = first - second
+print(summa, diff)
+third = 2
+mean = ((first + second + third) / 3)
+print(mean)
+
+first_string = 'Вторник'
+second_string = 'Понедельник'
+print(second_string + ',' + first_string)
+
+a = 2
+b = 3
+c = 4
+f = (a*b) + (a*c)
+print(f**3 / 2)
